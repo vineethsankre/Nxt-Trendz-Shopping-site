67fb409bcecfe38487ae48cc464b15b6fc873510
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-04-27 17:35:58 +0530

    Update App.js
    Removed extra <BrowserRouter>

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={LoginForm} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={LoginForm} />
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App
