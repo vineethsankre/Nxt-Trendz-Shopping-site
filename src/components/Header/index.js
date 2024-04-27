35af4c369fb8ae14400823eaca0cdb0f31f51357
vineethsankre <lsvineethsankre@gmail.com>
2024-04-27 09:37:30 +0000

    header component added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,35 @@
-// Write your JS code here
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <img
+        className="website-logo"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+        alt="website logo"
+      />
+      <ul className="nav-menu">
+        <li>
+            Home
+        </li>
+        <li>
+            Products
+        </li>
+        <li>
+            Cart
+        </li>
+      </ul>
+      <button type="button" className="logout-desktop-btn">
+        Logout
+      </button>
+      <button type="button" className="logout-mobile-btn">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+          alt="logout icon"
+          className="logout-icon"
+        />
+      </button>
+    </div>
+  </nav>
+)
+export default Header
