e09ff7830de2b1adfb74cefc9c4c5c72167e80e9
vineethsankre <lsvineethsankre@gmail.com>
2024-04-27 09:39:21 +0000

    Added Home and NotFound paths to Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+import './App.css'
 
 const App = () => (
-
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
