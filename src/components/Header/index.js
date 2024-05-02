e5340ce1a2f54ad0e584ca2e786930576fe1e024
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-05-02 21:54:01 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,17 @@
 import {Link, withRouter} from 'react-router-dom'
+
 import Cookies from 'js-cookie'
+
 import './index.css'
 
 const Header = props => {
   const onClickLogout = () => {
     const {history} = props
+
     Cookies.remove('jwt_token')
     history.replace('/login')
   }
+
   return (
     <nav className="nav-header">
       <div className="nav-content">
@@ -18,7 +22,11 @@ const Header = props => {
             alt="website logo"
           />
 
-          <button type="button" className="nav-mobile-btn">
+          <button
+            type="button"
+            className="nav-mobile-btn"
+            onClick={onClickLogout}
+          >
             <img
               src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
               alt="nav logout"
@@ -27,18 +35,30 @@ const Header = props => {
           </button>
         </div>
 
-        <div className="nav-content nav-bar-large-container">
+        <div className="nav-bar-large-container">
           <img
             className="website-logo"
             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
             alt="website logo"
           />
           <ul className="nav-menu">
-            <li className="nav-menu-item">Home</li>
+            <li className="nav-menu-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
 
-            <li className="nav-menu-item">Products</li>
+            <li className="nav-menu-item">
+              <Link to="/products" className="nav-link">
+                Products
+              </Link>
+            </li>
 
-            <li className="nav-menu-item">Cart</li>
+            <li className="nav-menu-item">
+              <Link to="/cart" className="nav-link">
+                Cart
+              </Link>
+            </li>
           </ul>
           <button
             type="button"
@@ -52,27 +72,32 @@ const Header = props => {
       <div className="nav-menu-mobile">
         <ul className="nav-menu-list-mobile">
           <li className="nav-menu-item-mobile">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
-              alt="nav home"
-              className="nav-bar-img"
-            />
+            <Link to="/" className="nav-link">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+                alt="nav home"
+                className="nav-bar-img"
+              />
+            </Link>
           </li>
 
           <li className="nav-menu-item-mobile">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
-              alt="nav products"
-              className="nav-bar-img"
-            />
+            <Link to="/products" className="nav-link">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+                alt="nav products"
+                className="nav-bar-img"
+              />
+            </Link>
           </li>
-
           <li className="nav-menu-item-mobile">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-              alt="nav cart"
-              className="nav-bar-img"
-            />
+            <Link to="/cart" className="nav-link">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+                alt="nav cart"
+                className="nav-bar-img"
+              />
+            </Link>
           </li>
         </ul>
       </div>
