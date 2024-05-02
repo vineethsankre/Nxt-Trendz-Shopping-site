94b7867a4c0bd47ba7600446871de03bc14491f1
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-05-02 21:59:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
 
 import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
 import LoginForm from './components/LoginForm'
 import NotFound from './components/NotFound'
 
@@ -8,8 +11,9 @@ import './App.css'
 
 const App = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
     <Route component={NotFound} />
   </Switch>
 )
