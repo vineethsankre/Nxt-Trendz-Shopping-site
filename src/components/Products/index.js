1f238f47864f65f70c2393cd131941483c6406cb
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-05-03 22:57:26 +0530

    Update index.js

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,3 +1,6 @@
+import AllProductsSection from '../AllProductsSection'
+import PrimeDealsSection from '../PrimeDealsSection'
+
 import Header from '../Header'
 
 import './index.css'
@@ -5,12 +8,9 @@ import './index.css'
 const Products = () => (
   <>
     <Header />
-    <div className="products-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
-        alt="products"
-        className="products-img"
-      />
+    <div className="product-sections">
+      <PrimeDealsSection />
+      <AllProductsSection />
     </div>
   </>
 )
