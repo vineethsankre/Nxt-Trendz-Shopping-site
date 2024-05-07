9eb504405f6f0ee11710f0cd15377734851f159a
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-05-07 23:32:19 +0530

    Update index.js

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import './index.css'
 
 const ProductCard = props => {
@@ -5,22 +6,25 @@ const ProductCard = props => {
   const {title, brand, imageUrl, rating, price} = productData
 
   return (
-    <li className="product-item">
-      <img src={imageUrl} alt="product" className="thumbnail" />
-      <h1 className="title">{title}</h1>
-      <p className="brand">by {brand}</p>
-      <div className="product-details">
-        <p className="price">Rs {price}/-</p>
-        <div className="rating-container">
-          <p className="rating">{rating}</p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-            alt="star"
-            className="star"
-          />
+    <Link to={`/products/${id}`} className="item-link">
+      <li className="product-item">
+        <img src={imageUrl} alt="product" className="thumbnail" />
+        <h1 className="title">{title}</h1>
+        <p className="brand">by {brand}</p>
+        <div className="product-details">
+          <p className="price">Rs {price}/-</p>
+          <div className="rating-container">
+            <p className="rating">{rating}</p>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+              alt="star"
+              className="star"
+            />
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   )
+}
 }
 export default ProductCard
