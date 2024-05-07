29f010f5c4d316ca3f5312fc4de8a3dd6027b5db
vineethsankre <113631587+vineethsankre@users.noreply.github.com>
2024-05-07 23:33:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 
 import Home from './components/Home'
 import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
 import Cart from './components/Cart'
 import LoginForm from './components/LoginForm'
 import NotFound from './components/NotFound'
@@ -14,6 +15,7 @@ const App = () => (
     <Route exact path="/login" component={LoginForm} />
     <ProtectedRoute exact path="/" component={Home} />
     <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/products/:id" component={ProductItemDetails} />
     <ProtectedRoute exact path="/cart" component={Cart} />
     <Route path="/not-found" component={NotFound} />
     <Redirect to="not-found" />
